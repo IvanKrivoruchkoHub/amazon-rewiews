4813cb7a31c3a1ef03ec8ec42b39f8a17bd30bb8
ivan <Ivan.krivoruchko.1999@gmail.com>
2020-03-03 12:24:30 +0200

    first commit

---
+++ src/main/java/com/example/amazonrewies/AmazonRewiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.amazonrewies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonRewiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonRewiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amazonrewies/AmazonRewiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazonrewies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonRewiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
